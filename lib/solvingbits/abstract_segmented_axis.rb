c001ba5fea5aabc8375605f0807c645b66bc90e2
Mike Bowler <mbowler@GargoyleSoftware.com>
2019-05-03 18:23:15 -0600

    cleanup

--- lib/solvingbits/abstract_segmented_axis.rb
+++ lib/solvingbits/abstract_segmented_axis.rb
@@ -9,13 +9,6 @@ module SolvingBits
     attr_configurable :segments_width_px, defaults_to: 10
     attr_configurable :segments_font_size_px, defaults_to: 13
 
-    # attr_configurable :formatter
-
-    # attr_configurable :label_text
-    # attr_configurable :label_visible, defaults_to: false, only: [true, false]
-    # attr_configurable :label_font_size_px, defaults_to: 13
-
-
     def initialize params
       initialize_configuration params: params
     end
