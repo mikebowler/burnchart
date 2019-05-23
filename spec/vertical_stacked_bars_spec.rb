50a341b9b5cc2d732c9ad58e1903d41c58a5d1db
Mike Bowler <mbowler@GargoyleSoftware.com>
2019-05-23 19:37:56 -0400

    Added context

--- spec/vertical_stacked_bars_spec.rb
+++ spec/vertical_stacked_bars_spec.rb
@@ -1,44 +1,45 @@
 # frozen_string_literal: true
 
 module SolvingBits
-  RSpec.describe VerticalStackedBars do
-    it 'should render a single data point' do
-      component = VerticalStackedBars.new
-      component.create_stack do |stack|
-        stack << StackItem.new(value: 5, label: 'committed', color: 'blue')
+  RSpec.describe StackedBars do
+    context 'vertical' do
+      it 'should render a single data point' do
+        component = VerticalStackedBars.new
+        component.create_stack do |stack|
+          stack << StackItem.new(value: 5, label: 'committed', color: 'blue')
+        end
+
+        expect(component.preferred_size).to eq(Size.new(height: 5, width: 10))
+
+        expect(component.to_svg(:partial)).to eq(
+          "<rect x='0' y='0' width='10' height='5' style='fill: blue'/>"
+        )
       end
 
-      expect(component.preferred_size).to eq(Size.new(height: 5, width: 10))
+      it 'should render multiple stacks with multiple items per stack' do
+        component = VerticalStackedBars.new
+        component.create_stack do |stack|
+          stack << StackItem.new(value: 5, label: 'committed', color: 'blue')
+          stack << StackItem.new(value: 5, label: 'added', color: 'pink')
+        end
+        component.create_stack do |stack|
+          stack << StackItem.new(value: 5, label: 'completed', color: 'green')
+          stack << StackItem.new(value: 5, label: 'removed', color: 'orange')
+          stack << StackItem.new(value: 5, label: 'incomplete', color: 'black')
+        end
 
-      expect(component.to_svg(:partial)).to eq(
-        "<rect x='0' y='0' width='10' height='5' style='fill: blue'/>"
-      )
+        # File.open 'stacked.svg', 'w' do |file|
+        #   file.puts component.to_svg
+        # end
+        # component.dump
+        expect(component.to_svg(:partial)).to eq(
+          "<rect x='0' y='10' width='10' height='5' style='fill: blue'/>" \
+          "<rect x='0' y='5' width='10' height='5' style='fill: pink'/>" \
+          "<rect x='10' y='10' width='10' height='5' style='fill: green'/>" \
+          "<rect x='10' y='5' width='10' height='5' style='fill: orange'/>" \
+          "<rect x='10' y='0' width='10' height='5' style='fill: black'/>"
+        )
+      end
     end
-
-    it 'should render multiple stacks with multiple items per stack' do
-      component = VerticalStackedBars.new
-      component.create_stack do |stack|
-        stack << StackItem.new(value: 5, label: 'committed', color: 'blue')
-        stack << StackItem.new(value: 5, label: 'added', color: 'pink')
-      end
-      component.create_stack do |stack|
-        stack << StackItem.new(value: 5, label: 'completed', color: 'green')
-        stack << StackItem.new(value: 5, label: 'removed', color: 'orange')
-        stack << StackItem.new(value: 5, label: 'incomplete', color: 'black')
-      end
-
-      # File.open 'stacked.svg', 'w' do |file|
-      #   file.puts component.to_svg
-      # end
-      # component.dump
-      expect(component.to_svg(:partial)).to eq(
-        "<rect x='0' y='10' width='10' height='5' style='fill: blue'/>" \
-        "<rect x='0' y='5' width='10' height='5' style='fill: pink'/>" \
-        "<rect x='10' y='10' width='10' height='5' style='fill: green'/>" \
-        "<rect x='10' y='5' width='10' height='5' style='fill: orange'/>" \
-        "<rect x='10' y='0' width='10' height='5' style='fill: black'/>"
-      )
-    end
-
   end
 end
